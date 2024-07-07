4bd0e7860f0d4f2debd34e85abca9105e45d910e
Prathesh Manickkam <133184140+PratheshManickkam2295@users.noreply.github.com>
2024-07-07 09:07:43 +0530

    update

--- Controllers/Todos.Controllers.js
+++ Controllers/Todos.Controllers.js
@@ -1,6 +1,13 @@
 const TodosRouter = require("express").Router();
 
 // HTTP METHODS = GET, POST, PUT, DELETE
+TodosRouter.get("/", (request, response, next) => {
+  const data = Array.from(require("../mocks/todos.json"));
+  return response.status(200).json({
+    result: data,
+    length: data.length,
+  });
+});
 
 TodosRouter.get("/:id", (request, response, next) => {
   const { id } = request.params;
@@ -13,3 +20,19 @@ TodosRouter.get("/:id", (request, response, next) => {
 });
 
 module.exports = TodosRouter;
+
+// const TodosRouter = require("express").Router();
+
+// // HTTP METHODS = GET, POST, PUT, DELETE
+
+// TodosRouter.get("/:id", (request, response, next) => {
+//   const { id } = request.params;
+//   const data = Array.from(require("../mocks/todos.json"));
+//   const filteredData = data.filter((item) => Number(item.id) === Number(id));
+//   return response.status(200).json({
+//     result: filteredData,
+//     length: filteredData.length,
+//   });
+// });
+
+// module.exports = TodosRouter;
